3dd46be18767486ca15862cb423476d031cc9a8a
lukechampine <luke.champine@gmail.com>
2020-06-17 16:00:31 -0400

    fix tile colors in light mode

--- tile/tile.js
+++ tile/tile.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export function roteTile() {
   return (
-    <div className={"w-100 h-100 relative bg-black bg-gray0-d ba b--black b--gray1-d"}>
+    <div className={"w-100 h-100 relative bg-white bg-gray0-d ba b--black b--gray1-d"}>
       <a className="w-100 h-100 db bn" href="/~rote">
-        <p className="white f9 absolute" style={{ left: 8, top: 8 }}>
+        <p className="black white-d f9 absolute" style={{ left: 8, top: 8 }}>
            Flashcards
         </p>
         <img className="absolute invert-d" src="/~rote/img/Tile.png"
